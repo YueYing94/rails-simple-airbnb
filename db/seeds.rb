eae0595b04dd03221ed3b4f969578befa3363a76
yue ying <sarahyingyue1994@gmail.com>
2023-05-18 23:57:15 +0200

    /

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+
+Flat.create!(
+  name: 'sea side flat',
+  address: 'scheveningen',
+  description: 'with a big balcony with nice warm sea breeze',
+  price_per_night: 200,
+  number_of_guests: 6
+)
+
+
+Flat.create!(
+  name: 'downtown skyscraper',
+  address: 'melbourne cbd',
+  description: 'with good city view',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+
+
+Flat.create!(
+  name: 'Easy studio',
+  address: 'rotterdam centraal',
+  description: 'good location directly at the train station',
+  price_per_night: 60,
+  number_of_guests: 1
+)
